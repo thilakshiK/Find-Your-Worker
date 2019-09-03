17bfcb366042801a433183f32dbfd4f6960edead
Thilakshi Fonseka <thilakshi_f@3logixs.lk>
2019-09-03 18:42:42 +0530

    add routes to login component

--- src/components/Login/login.jsx
+++ src/components/Login/login.jsx
@@ -1,17 +1,90 @@
 import React from "react";
-import { Button, Form, FormGroup, Input } from "reactstrap";
+import { Button, Form, FormGroup, Input , Alert} from "reactstrap";
 import "../../assets/styles/font.css";
 //import "../simplenav";
 import SimpleNavComponent from "../Navbar/simplenav";
-import { Link   } from "react-router-dom";
+import { Link } from "react-router-dom";
+import axios from "axios";
 
 export default class LoginFormComponent extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: "",
+      password: "",
+      visibleFailure : false
+    };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleEmailChange = this.handleEmailChange.bind(this);
+    this.handlePasswordChange = this.handlePasswordChange.bind(this);
+  }
+
+  handleEmailChange(event) {
+    this.setState({ email: event.target.value });
+  }
+
+  handlePasswordChange(event) {
+    this.setState({ password: event.target.value });
+  }
+
+  validateForm() {
+    return this.state.email.length > 0 && this.state.password.length > 0;
+  }
+
+  // onDismissFailure(){
+  //   this.setState({visibleFailure :false});
+  // }
+
+  handleSubmit(event) {
+    event.preventDefault();
+
+    const loginDetails = {
+      UserEmail: this.state.email,
+      Password: this.state.password
+    };
+
+    axios
+      .post("http://127.0.0.1:3000/user/login", loginDetails)
+      .then(res => {
+        console.log(res);
+        
+        if (res.data.message === "Authorized") {
+          this.setState({
+            email : "",
+            password : ""
+  
+          });
+          localStorage.setItem('sessionType', res.data.result.sessionType);
+          localStorage.setItem("UserId" , res.data.result.UserId);
+          if(res.data.result.sessionType  == "worker"){
+            this.props.history.push({pathname : "/workerProfile"})
+
+          }else{
+            this.props.history.push({pathname : "/clientProfile"})
+
+          }
+         
+        }else{
+          this.setState({
+            email : "",
+            password : "",
+            visibleFailure : true
+          })
+        }
+      });
+  }
+
   render() {
     return (
       <div>
         <SimpleNavComponent />
-        
-        <Form style={{ margin: "auto", width: 400, marginTop: 100 }}>
+
+        <Form
+          style={{ margin: "auto", width: 400, marginTop: 100 }}
+          onSubmit={this.handleSubmit}
+        >
           <h3
             style={{
               fontFamily: "Josefin Sans",
@@ -24,22 +97,37 @@ export default class LoginFormComponent extends React.Component {
           <FormGroup>
             <Input
               type="email"
-              name="email"
-              id="exampleEmail"
+              value={this.state.email}
+              onChange={this.handleEmailChange}
               placeholder="Email"
+              required
             />
           </FormGroup>
           <FormGroup>
             <Input
               type="password"
-              name="password"
-              id="examplePassword"
+              value={this.state.password}
+              onChange={this.handlePasswordChange}
               placeholder="Password "
+              required
             />
           </FormGroup>
-          <Button color="success" style={{ marginLeft: 160, marginTop: 30 }}>
-            Login
-          </Button>
+
+          <Alert
+            color="danger"
+            isOpen={this.state.visibleFailure}
+            // toggle={this.onDismissFailure}
+          >
+            Invalid Credentials ! 
+          </Alert>
+
+          <Input
+            type="submit"
+            value="Login"
+            className="btn btn-success"
+            disabled={!this.validateForm()}
+            style={{ marginTop: 30 }}
+          ></Input>
 
           <h6
             style={{
