733d28ce6cf70dd87cafc2bc8b817f4eb9c453a8
Thilakshi Fonseka <thilakshi_f@3logixs.lk>
2019-09-03 15:17:53 +0530

    add routes to signup

--- src/components/Signup/signup.jsx
+++ src/components/Signup/signup.jsx
@@ -1,52 +1,105 @@
 import React from "react";
-import { Button, Form, FormGroup, Input, Label } from "reactstrap";
+import { Form, FormGroup, Input, Alert } from "reactstrap";
 import "../../assets/styles/font.css";
 import SimpleNavComponent from "../Navbar/simplenav";
 import { Link } from "react-router-dom";
-import axios from 'axios';
-export default class SingupFormComponent extends React.Component {
+import axios from "axios";
+import Select from "react-select";
 
+export default class SingupFormComponent extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      email : "",
-      password : "",
-      phonenumber : ""
-
+      email: "",
+      password: "",
+      phonenumber: "",
+      usertype: "",
+      visibleSuccess :false,
+      visibleFailure :false
     };
 
     this.handleEmailChange = this.handleEmailChange.bind(this);
     this.handlePasswordChange = this.handlePasswordChange.bind(this);
-    this.handlePhoneNumberChange = this.handlePasswordChange.bind(this);
-
-
+    this.handlePhoneNumberChange = this.handlePhoneNumberChange.bind(this);
+    this.handleSelect = this.handleSelect.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.onDismissSuccess = this.onDismissSuccess.bind(this);
+    this.onDismissFailure = this.onDismissFailure.bind(this);
   }
 
   handleEmailChange(event) {
-    this.setState({email : event.target.value});
+    this.setState({ email: event.target.value });
+  }
+
+  handlePasswordChange(event) {
+    this.setState({ password: event.target.value });
+  }
+
+  handlePhoneNumberChange(event) {
+    this.setState({ phonenumber: event.target.value });
+  }
+
+  handleSelect(usertype) {
+    this.setState({ usertype: usertype });
+  }
+
+  onDismissSuccess(){
+    this.setState({visibleSuccess :false});
+  }
+
+  onDismissFailure(){
+    this.setState({visibleFailure :false});
   }
 
   handleSubmit(event) {
-    //
+    event.preventDefault();
+
+    const registrationDetails = {
+      UserEmail: this.state.email,
+      Password: this.state.password,
+      ContactNumber: this.state.phonenumber,
+      UserType: this.state.usertype.value
+    };
+
+    axios
+      .post("http://127.0.0.1:3000/user/register", registrationDetails)
+      .then(res => {
+        if (res.data.message === "User Created") {
+          this.setState({
+            email: "",
+            password: "",
+            phonenumber: "",
+            usertype: "",
+            visibleSuccess :true
+          })
+         
+        } else {
+          this.setState({
+            email: "",
+            password: "",
+            phonenumber: "",
+            usertype: "",
+            visibleFailure : true
+           
+          });
+         
+        }
+      });
   }
-
-  handlePasswordChange (event) {
-    this.setState({password: event.target.value});
-  }
-
-  handlePhoneNumberChange(event){
-    this.setState({phonenumber : event.target.value});
-  }
-
-
-
 
   render() {
+    const options = [
+      { value: 0, label: "Client" },
+      { value: 1, label: "Worker" }
+    ];
     return (
       <div>
         <SimpleNavComponent />
-        
-        <Form style={{ margin: "auto", width: 400, marginTop: 100 }} onSubmit = {this.handleSubmit}>
+
+        <Form
+          style={{ margin: "auto", width: 400, marginTop: 100 }}
+          onSubmit={this.handleSubmit}
+        >
           <h3
             style={{
               fontFamily: "Josefin Sans",
@@ -56,13 +109,12 @@ export default class SingupFormComponent extends React.Component {
           >
             Get your free account
           </h3>
-          
 
           <FormGroup>
             <Input
               type="email"
-              value = {this.state.email}
-              onChange = {this.handleEmailChange}
+              value={this.state.email}
+              onChange={this.handleEmailChange}
               placeholder="Email"
               required
             />
@@ -70,37 +122,54 @@ export default class SingupFormComponent extends React.Component {
           <FormGroup>
             <Input
               type="password"
-              value = {this.state.password}
-              onChange = {this.handlePasswordChange}
+              value={this.state.password}
+              onChange={this.handlePasswordChange}
               placeholder="Password "
+              required
             />
           </FormGroup>
 
           <FormGroup>
             <Input
               type="number"
-              value = {this.state.phonenumber}
-              onChange = {this.handlePhoneNumberChange}
+              value={this.state.phonenumber}
+              onChange={this.handlePhoneNumberChange}
               placeholder="Contact Number"
+              required
             />
           </FormGroup>
 
-          <FormGroup check inline>
-            <Label check>
-              <Input type="radio" name="radio1" style={{ marginLeft: 50 }} />{" "}
-              Worker
-            </Label>
-          </FormGroup>
-          <FormGroup check inline>
-            <Label check>
-              <Input type="radio" name="radio1" style={{ marginLeft: 150 }} />
-              User
-            </Label>
+          <FormGroup>
+            <Select
+              options={options}
+              placeholder="User Type"
+              onChange={this.handleSelect}
+              value={this.state.usertype}
+            ></Select>
           </FormGroup>
 
-          <Button color="success" style={{ marginLeft: 140, marginTop: 30 }}>
-            Sign Me Up
-          </Button>
+          <Alert
+            color="success"
+            isOpen={this.state.visibleSuccess}
+            toggle={this.onDismissSuccess}
+          >
+            Registration Successfull ! Please Login to Continue
+          </Alert>
+
+          <Alert
+            color="danger"
+            isOpen={this.state.visibleFailure}
+            toggle={this.onDismissFailure}
+          >
+            User already exists ! 
+          </Alert>
+
+          <Input
+            type="submit"
+            value="Sign Me Up"
+            className="btn btn-success"
+            style={{ marginTop: 30 }}
+          ></Input>
 
           <h6
             style={{
