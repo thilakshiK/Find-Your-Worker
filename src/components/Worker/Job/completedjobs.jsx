ec13d784d3dcdfa3f9322d83e10d62f10bc42900
Thilakshi Fonseka <thilakshi_f@3logixs.lk>
2019-09-06 10:00:36 +0530

    add get route to completed jobs

--- src/components/Worker/Job/completedjobs.jsx
+++ src/components/Worker/Job/completedjobs.jsx
@@ -35,7 +35,7 @@ class CompletedJobComponent extends React.Component {
           onClick={this.toggle}
           style={{ width: 300, margin: 20 }}
         >
-          {this.props.jobid}
+          {this.props.obj.OrderId}
         </Button>
         <Modal
           isOpen={this.state.modal}
@@ -45,66 +45,141 @@ class CompletedJobComponent extends React.Component {
           <ModalHeader toggle={this.toggle}>Completed Job</ModalHeader>
           <ModalBody style={{ fontFamily: "Josefin Sans" }}>
             <Form>
+              {/* <FormGroup>
+                <Label for="createdDate">Created Date</Label>
+                <Input
+                  type="date"
+                  value={this.props.obj.CreatedDate.data}
+                  id="createdDate"
+                  disabled
+                />
+              </FormGroup> */}
+
               <FormGroup>
-                <Label for="clientName">Client Name</Label>
-                <Input type="name" name="name" id="clientName" disabled />
-              </FormGroup>
-              <FormGroup>
-                <Label for="location">Location</Label>
-                <Input type="location" name="location" id="location" disabled />
+                <Label for="clientFirstName">Client First Name</Label>
+                <Input
+                  type="text"
+                  value={this.props.obj.FirstName}
+                  id="clientFirstName"
+                  disabled
+                />
               </FormGroup>
 
               <FormGroup>
-                <Label for="date">Date</Label>
-                <Input type="todayDate" name="todayDate" id="date" disabled />
-                {/* date must be added automatically */}
+                <Label for="clientLastName">Client Last Name</Label>
+                <Input
+                  type="text"
+                  value={this.props.obj.LastName}
+                  id="clientLastName"
+                  disabled
+                />
+              </FormGroup>
+              <FormGroup>
+                <Label for="location">Order Location</Label>
+                <Input
+                  type="text"
+                  value={this.props.obj.OrderLoaction}
+                  id="location"
+                  disabled
+                />
+              </FormGroup>
+
+              <FormGroup>
+                <Label for="orderdate">Order Date</Label>
+                <Input
+                  type="text"
+                  value={this.props.obj.OrderDate}
+                  id="orderdate"
+                  disabled
+                />
               </FormGroup>
 
               <FormGroup>
                 <Label for="contactno">Contact Number</Label>
                 <Input
-                  type="contactnumber"
-                  name="contactnumber"
+                  type="number"
+                  value={this.props.obj.ContactNumber}
                   id="contactno"
                   disabled
                 />
               </FormGroup>
 
               <FormGroup>
+                {/* skill title */}
                 <Label for="skill">Job Type</Label>
-                <Input type="skillType" name="skillType" id="skill" disabled />
+                <Input
+                  type="text"
+                  value={this.props.obj.SkillTitle}
+                  id="skill"
+                  disabled
+                />
               </FormGroup>
 
               <FormGroup>
                 <Label for="hrate">Hourly Charge</Label>
                 <Input
-                  type="hourcharge"
-                  name="hourcharge"
+                  type="number"
+                  value={this.props.obj.HourlyCharge}
                   id="hrate"
                   disabled
                 />
               </FormGroup>
 
               <FormGroup>
+                <Label for="starttime">Start Time</Label>
+                <Input
+                  type="time"
+                  value={this.props.obj.StartTime}
+                  id="starttime"
+                  disabled
+                />
+              </FormGroup>
+
+              <FormGroup>
                 <Label for="endtime">End Time</Label>
-                <Input type="endTime" name="endTime" id="endtime" disabled />
+                <Input
+                  type="time"
+                  value={this.props.obj.EndTime}
+                  id="endtime"
+                  disabled
+                />
+              </FormGroup>
+
+              <FormGroup>
+                <Label for="duration">Duration</Label>
+                <Input
+                  type="number"
+                  value={this.props.obj.Duration}
+                  id="duration"
+                  disabled
+                />
               </FormGroup>
 
               <FormGroup>
                 <Label for="finalprice">Final Price</Label>
-                <Input type="price" name="price" id="finalprice" disabled />
+                <Input
+                  type="number"
+                  value={this.props.obj.FinalPrice}
+                  id="finalprice"
+                  disabled
+                />
               </FormGroup>
 
               <FormGroup>
                 <Label for="rating">Rating</Label>
-                <Input type="rate" name="rate" id="rating" disabled />
+                <Input
+                  type="number"
+                  value={this.props.obj.Rate}
+                  id="rating"
+                  disabled
+                />
               </FormGroup>
 
               <FormGroup>
                 <Label for="feedback">Feedback</Label>
                 <Input
                   type="textarea"
-                  name="feedbacktext"
+                  value={this.props.obj.Review}
                   id="feedback"
                   disabled
                 />
